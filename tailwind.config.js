f98a26be5305aea0349d9433b5dc3cd8c5654e0e
María Rosa Aravid Martín <maravid@uoc.edu>
2024-05-28 19:26:13 +0200

    Changes in styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,44 @@ module.exports = {
       boxShadow: {
         'custom-neon': '0 0px 20px, 0 0 2px, 0 0 1em #4470ff, 0 0 0.5em #4470ff, 0 0 0.1em #4470ff, 0 10px 3px #000000',
       },
+      keyframes: {
+        blink: {
+          '78%': {
+            color: 'inherit',
+            textShadow: 'inherit',
+          },
+          '79%': {
+            color: '#333333',
+          },
+          '80%': {
+            textShadow: 'none',
+          },
+          '81%': {
+            color: 'inherit',
+            textShadow: 'inherit',
+          },
+          '82%': {
+            color: '#333333',
+            textShadow: 'none',
+          },
+          '83%': {
+            color: 'inherit',
+            textShadow: 'inherit',
+          },
+          '92%': {
+            color: '#333333',
+            textShadow: 'none',
+          },
+          '92.5%': {
+            color: 'inherit',
+            textShadow: 'inherit',
+          },
+        },
+      },
+      animation: {
+        blink2: 'blink 2s linear infinite',
+        blink3: 'blink 3s linear infinite',
+      },
     },
   },
   plugins: [
